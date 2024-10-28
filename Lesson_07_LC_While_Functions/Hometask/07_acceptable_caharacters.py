e1672e343d6e60c0869436590e68b5880e099e41
antohaUa <antoha.ua@gmail.com>
2024-10-28 21:45:42 +0200

    Lesson_07 task examples

---
+++ Lesson_07_LC_While_Functions/Hometask/07_acceptable_caharacters.py
@@ -0,0 +1,6 @@
+# Given string  of characters
+# Check that all characters are from ascii table
+# starting from space character(32) and finishing ~ character(126)
+# return true or false
+s1 = '🦊 sdf45435324 🦆'  # -> False
+s2 = 'Super String~'  # -> true
---
+++ Lesson_07_LC_While_Functions/Hometask/07_multiplication_table.py
@@ -0,0 +1,10 @@
+# Given 0 < number (integer) <= 11
+# You need to print multiplication table based on this number
+#
+# Example: num=5
+# Output:
+#  1   2   3   4   5
+#  2   4   6   8  10
+#  3   6   9  12  15
+#  4   8  12  16  20
+#  5  10  15  20  25
